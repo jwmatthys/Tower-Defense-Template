c7333858484096e024b717651cef2b1892e8a3e0
Joel Matthys <jwmatthys@yahoo.com>
2026-04-28 09:48:56 -0500

    Move grid to 0 position & make freeze more efficient

--- Assets/Scripts/Enemy Scripts/EnemyFreezer.cs
+++ Assets/Scripts/Enemy Scripts/EnemyFreezer.cs
@@ -3,19 +3,20 @@ using UnityEngine;
 
 public class EnemyFreezer : MonoBehaviour
 {
-    public float fireRate = 15f;
-    public float attackRadius = 3f;
+    public float freezeInterval = 15f;
+    public float freezeRadius = 3f;
     public float freezeDuration = 5f;
 
     private Coroutine _freezingCoroutine;
+    private readonly Collider[] _hits = new Collider[50];
 
     private void Update()
     {
-        Collider[] hits = Physics.OverlapSphere(transform.position, attackRadius, LayerMask.GetMask("Tower"));
+        int count = Physics.OverlapSphereNonAlloc(transform.position, freezeRadius, _hits, LayerMask.GetMask("Tower"));
 
-        if (hits.Length > 0 && _freezingCoroutine == null)
+        if (count > 0 && _freezingCoroutine == null)
             _freezingCoroutine = StartCoroutine(FreezeRoutine());
-        else if (hits.Length == 0 && _freezingCoroutine != null)
+        else if (count == 0 && _freezingCoroutine != null)
         {
             StopCoroutine(_freezingCoroutine);
             _freezingCoroutine = null;
@@ -26,20 +27,15 @@ public class EnemyFreezer : MonoBehaviour
     {
         while (true)
         {
-            Debug.Log("Freezing!");
             FreezeAll();
-            yield return new WaitForSeconds(fireRate);
+            yield return new WaitForSeconds(freezeInterval);
         }
     }
 
     private void FreezeAll()
     {
-        Collider[] hits = Physics.OverlapSphere(transform.position, attackRadius, LayerMask.GetMask("Tower"));
-
-        foreach (Collider hit in hits)
-        {
-            Debug.Log(hit.transform.name);
-            hit.GetComponent<TowerFrozen>()?.Freeze(freezeDuration);
-        }
+        int count = Physics.OverlapSphereNonAlloc(transform.position, freezeRadius, _hits, LayerMask.GetMask("Tower"));
+        for (int i = 0; i < count; i++)
+            _hits[i].GetComponent<TowerFrozen>()?.Freeze(freezeDuration);
     }
 }
